2d8b786fd54294078fc55a8856f7511e13bd9fc2
AndrasCirce <37252472+AndrasCirce@users.noreply.github.com>
2019-01-26 16:18:44 -0600

    Add files via upload

---
+++ simulacion/src/proyecto1/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+/**
+ *
+ * @author haru
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        sim_1 a=new sim_1();
+        a.setVisible(true);
+    }
+}
